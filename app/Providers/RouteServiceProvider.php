fa889af0c3e7340d2912b123151f911967a6b237
hcruz@solserin.com <hcruz@solserin.com>
2019-09-10 14:47:50 -0600

    antes de migraciones

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -65,9 +65,14 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::prefix('api')
+        //este era el original
+        /*Route::prefix('api')
              ->middleware('api')
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
+             */
+            Route::middleware('api')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/api.php'));
     }
 }
